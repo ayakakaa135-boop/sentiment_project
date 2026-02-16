3c253ad411eeb1f1cb5e366f462faa2b7dee6e3f
aya zartit <ayakakaa135@gmail.com>
2026-02-16 16:20:12 +0100

    Update urls.py

--- sentiment_project/urls.py
+++ sentiment_project/urls.py
@@ -1,7 +1,18 @@
+# Main URLs configuration with i18n
+# sentiment_project/urls.py
+
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
+    # Language switcher (without prefix)
+    path('i18n/', include('django.conf.urls.i18n')),
+]
+
+# URLs with language prefix
+urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('sentiment_app.urls')),
-]
+    prefix_default_language=True,  # Set to False if you don't want /en/ prefix for English
+)
